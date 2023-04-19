463d2f04850b128a1f1a37594c40519258f50770
AbhiAbhilashGowda <abhilashhs14@gmail.com>
2023-04-19 10:34:28 +0530

    All the code

--- src/test/java/com/automation/runner/Runner.java
+++ src/test/java/com/automation/runner/Runner.java
@@ -7,15 +7,15 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {// "classpath:FeatureFiles/Login.feature",
-		                   // "classpath:FeatureFiles/Mentorshipp.feature",
-                             // "classpath:FeatureFiles/Jobs.feature",
+@CucumberOptions(features = { "classpath:FeatureFiles/Login.feature",
+		                    "classpath:FeatureFiles/Mentorshipp.feature",
+                              "classpath:FeatureFiles/Jobs.feature",
                              //"classpath:FeatureFiles/InvalidLogin.feature",
-                           // "classpath:FeatureFiles/Courses.feature",
-                          // "classpath:FeatureFiles/More.feature",
-		                  // "classpath:FeatureFiles/ProfileUpload.feature",
+                            "classpath:FeatureFiles/Courses.feature",
+                           "classpath:FeatureFiles/More.feature",
+		                   "classpath:FeatureFiles/ProfileUpload.feature",
                          "classpath:FeatureFiles/Register.feature",
-                        // "classpath:FeatureFiles/JObsSideMap.feature",
+                         "classpath:FeatureFiles/JObsSideMap.feature",
 }, 
 glue = { "classpath:com.automation.stepdef"}, 
    monochrome = true,
